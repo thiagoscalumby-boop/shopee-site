e420162edd963cc36b80d6975a98816d8ce948ba
thiagoscalumby-boop <thiagoscalumby@gmail.com>
2026-04-07 02:25:46 -0300

    Create page.tsx

---
+++ app/erro/page.tsx
@@ -0,0 +1,7 @@
+export default function Erro() {
+  return (
+    <div style={{ background: "#111", color: "#fff", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>❌ Pagamento não concluído.</h1>
+    </div>
+  );
+}
