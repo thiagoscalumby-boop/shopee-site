9bbba02c103e95358ae589a2dab44064d41c2fac
thiagoscalumby-boop <thiagoscalumby@gmail.com>
2026-04-07 02:01:32 -0300

    Create page.tsx

---
+++ app/sucesso/page.tsx
@@ -0,0 +1,7 @@
+export default function Sucesso() {
+  return (
+    <div style={{ background: "#111", color: "#fff", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>✅ Pagamento aprovado! Acesso liberado.</h1>
+    </div>
+  );
+}
